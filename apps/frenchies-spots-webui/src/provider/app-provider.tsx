f6bc2d389ae4e923a4b388b5bd696c97c48f68c4
JF-PS <jfps.dev21@gmail.com>
2023-09-26 00:45:45 +0200

    t

--- apps/frenchies-spots-webui/src/provider/app-provider.tsx
+++ apps/frenchies-spots-webui/src/provider/app-provider.tsx
@@ -16,7 +16,7 @@ export function AppProvider({ children }: AppProviderProps) {
       <AuthProvider>
         <FrSpotsMapProvider>
           <LocationProvider>
-            <FrSpotsMaterialProvider>{children}</FrSpotsMaterialProvider>
+            <FrSpotsMaterialProvider><>{children as ReactNode}</></FrSpotsMaterialProvider>
           </LocationProvider>
         </FrSpotsMapProvider>
       </AuthProvider>
